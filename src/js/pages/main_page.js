86549ab6c6b0d2d60031f6a0cb7f72162647a66a
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-06-16 20:12:57 +0300

    added: main page, not adapted

--- src/js/pages/main_page.js
+++ src/js/pages/main_page.js
@@ -1,7 +1,8 @@
+// INTRO
 const vanilBtn = document.querySelector('#main_page-intro_slider-vanil_btn')
 const raspberryBtn = document.querySelector('#main_page-intro_slider-raspberry_btn')
 
-const mainPagePartnersSlider = new Swiper('#main_page-intro_slider', {
+const mainPageIntroSlider = new Swiper('#main_page-intro_slider', {
     speed: 1500,
     autoplay: {
         delay: 3000
@@ -21,19 +22,20 @@ const mainPagePartnersSlider = new Swiper('#main_page-intro_slider', {
 });
 
 vanilBtn.addEventListener('click',()=>{
-    mainPagePartnersSlider.slideTo(0)
+    mainPageIntroSlider.slideTo(0)
 
     raspberryBtn.classList.remove('btns__item-raspberry-active')
     vanilBtn.classList.add('btns__item-vanil-active')
 })
 
 raspberryBtn.addEventListener('click',()=>{
-    mainPagePartnersSlider.slideTo(1)
+    mainPageIntroSlider.slideTo(1)
 
     vanilBtn.classList.remove('btns__item-vanil-active')
     raspberryBtn.classList.add('btns__item-raspberry-active')
 })
 
+// WARNING
 const mainPageFaringazonWarning = document.querySelector('#main_page-faringazon_warning')
 const mainPageFaringazonWarningCloseBtn = mainPageFaringazonWarning.querySelector('.faringazon_warning__btn')
 
@@ -44,3 +46,11 @@ mainPageFaringazonWarningCloseBtn.addEventListener('click',()=>{
         mainPageFaringazonWarning.style = 'display: none;'
     },1000)
 })
+
+// ARTICLES
+
+const mainPageArticlesSlider = new Swiper('#main_page-articles_slider', {
+    speed: 1500,
+    slidesPerView: 3.5,
+    spaceBetween: 24
+});
