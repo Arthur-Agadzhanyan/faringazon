9d1fde6b3eb6eaf2e5ba0648104e8df12990391d
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-06-17 01:05:00 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -40,10 +40,11 @@ function createSlider(sliderSelector, userOptions={}) {
     return new Swiper(sliderSelector, {...sliderOptions, ...userOptions});
 }
 
+// INTRO
 const vanilBtn = document.querySelector('#main_page-intro_slider-vanil_btn')
 const raspberryBtn = document.querySelector('#main_page-intro_slider-raspberry_btn')
 
-const mainPagePartnersSlider = new Swiper('#main_page-intro_slider', {
+const mainPageIntroSlider = new Swiper('#main_page-intro_slider', {
     speed: 1500,
     autoplay: {
         delay: 3000
@@ -63,19 +64,20 @@ const mainPagePartnersSlider = new Swiper('#main_page-intro_slider', {
 });
 
 vanilBtn.addEventListener('click',()=>{
-    mainPagePartnersSlider.slideTo(0)
+    mainPageIntroSlider.slideTo(0)
 
     raspberryBtn.classList.remove('btns__item-raspberry-active')
     vanilBtn.classList.add('btns__item-vanil-active')
 })
 
 raspberryBtn.addEventListener('click',()=>{
-    mainPagePartnersSlider.slideTo(1)
+    mainPageIntroSlider.slideTo(1)
 
     vanilBtn.classList.remove('btns__item-vanil-active')
     raspberryBtn.classList.add('btns__item-raspberry-active')
 })
 
+// WARNING
 const mainPageFaringazonWarning = document.querySelector('#main_page-faringazon_warning')
 const mainPageFaringazonWarningCloseBtn = mainPageFaringazonWarning.querySelector('.faringazon_warning__btn')
 
@@ -85,7 +87,35 @@ mainPageFaringazonWarningCloseBtn.addEventListener('click',()=>{
     setTimeout(()=>{
         mainPageFaringazonWarning.style = 'display: none;'
     },1000)
-});
+})
+
+// ARTICLES
+
+const mainPageArticlesSlider = new Swiper('#main_page-articles_slider', {
+    speed: 1500,
+
+    spaceBetween: 24,
+    breakpoints:{
+        320:{
+            slidesPerView: 1.2,
+        },
+        600:{
+            slidesPerView: 1.4,
+        },
+        700:{
+            slidesPerView: 1.8,
+        },
+        1000:{
+            slidesPerView: 2.4,
+        },
+        1200:{
+            slidesPerView: 2.8,
+        },
+        1500:{
+            slidesPerView: 3.5,
+        }
+    }
+});;
 
 AOS.init()
 
