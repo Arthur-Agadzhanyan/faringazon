2a9c8bcc0bad71b66202913b54841b8d91196754
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-06-18 13:57:55 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -115,7 +115,33 @@ const mainPageArticlesSlider = new Swiper('#main_page-articles_slider', {
             slidesPerView: 3.5,
         }
     }
-});;
+});
+
+// USAGE
+function usageScript(){
+    const usageAdultBtn = document.querySelector('#usage_btn_adult')
+    const usageKidsBtn = document.querySelector('#usage_btn_kids')
+
+    const usageAdultList = document.querySelector('#usage_adult_list')
+    const usageKidsList = document.querySelector('#usage_kids_list')
+
+    usageAdultBtn.addEventListener('click',(e)=>{
+        e.target.classList.add('button-mans-active')
+        usageKidsBtn.classList.remove('button-kids-active')
+
+        usageAdultList.classList.remove('dn')
+        usageKidsList.classList.add('dn')
+    })
+
+    usageKidsBtn.addEventListener('click',(e)=>{
+        e.target.classList.add('button-kids-active')
+        usageAdultBtn.classList.remove('button-mans-active')
+
+        usageAdultList.classList.add('dn')
+        usageKidsList.classList.remove('dn')
+    })
+}
+usageScript();
 
 AOS.init()
 
@@ -127,3 +153,53 @@ navbarBtn.addEventListener('click',(e)=>{
     navbarPoppup.classList.toggle('mb_bar_closed')
     navHeader.classList.toggle('mb_header-active')
 })
+
+// MODAL
+const questionBtns = document.querySelectorAll('.show_question_modal_btn')
+const questionModal = document.getElementById("questionModal")
+
+if(questionBtns.length && questionModal){
+    const modalDiv = questionModal.querySelectorAll('.form__div')
+
+    modalDiv.forEach(block=>{
+        let blockInput = block.querySelector('.form__input')
+        let blockLabel = block.querySelector('.form__label')
+
+        blockInput.addEventListener('focus',(e)=>{
+            blockInput.classList.add('focusedInput')
+            blockLabel.classList.add('focusedLabel')
+        })
+
+        blockInput.addEventListener('blur',()=>{
+            if(blockInput.value.length < 1){
+                blockInput.classList.remove('focusedInput')
+                blockLabel.classList.remove('focusedLabel')
+            }
+        })
+    })
+
+    questionBtns.forEach((btn)=>{
+        btn.addEventListener('click',(e)=>{
+            questionModal.style.display = "flex"
+            document.body.style.overflow = "hidden"
+            setTimeout(()=>questionModal.style.opacity = "1",50)
+
+            return
+        })
+    })
+}
+
+// CLOSE MODALS
+const closeModal = document.querySelectorAll(".close_modal")
+
+if(closeModal){
+    closeModal.forEach(el=>{
+        el.addEventListener("click",()=>{
+            if(questionModal){
+                questionModal.style.opacity = "0"
+                document.body.style.overflow = "auto"
+                setTimeout(()=>questionModal.style.display = "none",300)
+            }
+        })
+    })
+}
